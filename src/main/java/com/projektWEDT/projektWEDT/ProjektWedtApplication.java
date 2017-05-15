3f92dca17abcca4b8be556eca0fe1140f53d1f4c
Mikołaj Płachta <mixxenxd@wp.pl>
2017-05-15 21:47:02 +0200

    Początkowy szkielet aplikacji.

---
+++ src/main/java/com/projektWEDT/projektWEDT/ProjektWedtApplication.java
@@ -0,0 +1,12 @@
+package com.projektWEDT.projektWEDT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektWedtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektWedtApplication.class, args);
+	}
+}
