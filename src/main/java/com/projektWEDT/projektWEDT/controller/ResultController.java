13dc52775fb80c3ce480ed3b3a54690ad82d6739
Mikołaj Płachta <mixxenxd@wp.pl>
2017-05-21 18:38:52 +0200

    Dodanie widoków html - początkowy do ładowania pliku oraz końcowy z rezultatem.

--- src/main/java/com/projektWEDT/projektWEDT/controller/ResultController.java
+++ src/main/java/com/projektWEDT/projektWEDT/controller/ResultController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HomeController {
+public class ResultController {
 
-	@RequestMapping("/")
+	@RequestMapping("/result")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "index";
+        return "result";
     }
 }
