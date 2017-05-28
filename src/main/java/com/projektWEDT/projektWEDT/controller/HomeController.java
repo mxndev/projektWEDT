f19761e6d7387abd2802b4c61be2cdd2a162f0f1
Mariogd92 <mariusz.pajaczkowski92@gmail.com>
2017-05-28 12:08:55 +0200

    Dodanie czytania plików PDF wraz z dodaniem biblioteki i zależności. Doprowadzenie do sytuacji gdy wczytany tekst znajduje się w tablicy stringów.

--- src/main/java/com/projektWEDT/projektWEDT/controller/HomeController.java
+++ src/main/java/com/projektWEDT/projektWEDT/controller/HomeController.java
@@ -1,7 +1,18 @@
 package com.projektWEDT.projektWEDT.controller;
 
+import java.awt.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccess;
+import org.apache.pdfbox.io.RandomAccessRead;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +21,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 @Controller
 public class HomeController {
+	
+	PDFParser parser = null;
+    String text = "";
+    PDFTextStripper stripper = null;
+    PDDocument pdoc = null;
+    COSDocument cdoc = null;
+    RandomAccess scratchFile = null;
 
 	@RequestMapping("/")
 	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
@@ -31,15 +48,66 @@ public class HomeController {
 		}
 
 		try {
-
-			// Get the file and save it somewhere
-			byte[] bytes = file.getBytes();
 			System.out.println("File is uploaded well: " + file.getOriginalFilename());
-			redirectAttributes.addAttribute("error", "false");
+			redirectAttributes.addFlashAttribute("error", "false");
+			File pdfFile = convertFile(file);
 			
+			
+			pdoc = PDDocument.load(pdfFile);
+			stripper = new PDFTextStripper();
+			text = stripper.getText(pdoc);
+			System.out.println(text);
+			pdoc.close();
+			
+			ArrayList<String> mar = new ArrayList<>();
+			
+			mar = findPartsWithEmptyLine(text);
+			
+		//	parser = new PDFParser(new FileInputStream(pdfFile));
+			
+			//parser.parse();
+	      //  cdoc = parser.getDocument();
+	      //  stripper = new PDFTextStripper();
+	      //  pdoc = new PDDocument(cdoc);
+	      //  text = stripper.getText(pdoc);
+	        
+			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return "redirect:/";
+	}
+
+	private File convertFile(MultipartFile file) throws IOException {
+
+		File convFile = new File(file.getOriginalFilename());
+		convFile.createNewFile();
+		FileOutputStream fos = new FileOutputStream(convFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convFile;
+
+	}
+	
+	/**
+	 * Bierze tekst wprost z pdf i wyszukuje części pooddzielanych pustymi liniami
+	 * 
+	 * @param text
+	 * @return
+	 */
+	private ArrayList<String> findPartsWithEmptyLine(String text){
+		
+		ArrayList<String> partedString = new ArrayList<>();
+		
+		String[] lines = text.split(System.getProperty("line.separator"));
+		
+		System.out.println(lines[1]);
+		System.out.println(lines[3]);
+		
+		return partedString;
+		
+		
+		
 	}
 }
